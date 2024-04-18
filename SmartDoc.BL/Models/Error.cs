1c3da557ddb420b64a040ca79853b7caf9e6743d
Hector Caballero <caballero.22.809@gmail.com>
2024-04-18 14:00:16 -0600

    Add Error and Result abstractions

---
+++ SmartDoc.BL/Models/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace SmartDoc.BL.Models;
+public record Error(string Code, string Name)
+{
+    public static Error None = new(string.Empty, string.Empty);
+    public static Error NullValue = new("Error.NullValue", "Null value was provided.");
+}
