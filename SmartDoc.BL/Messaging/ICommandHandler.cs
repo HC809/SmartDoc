4164a674c6b06cf57e785b7436431d749462fb23
Hector Caballero <caballero.22.809@gmail.com>
2024-04-18 14:01:40 -0600

    Add ICommand & ICommandHandler

---
+++ SmartDoc.BL/Messaging/ICommandHandler.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SmartDoc.BL.Models;
+
+namespace SmartDoc.BL.Messaging;
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result> where TCommand : ICommand { }
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>> where TCommand : ICommand<TResponse> { }
